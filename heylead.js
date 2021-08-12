0ce2a61f2fb585b7a10ef181e77938cb3b06d194
pratik1243 <87714664+pratik1243@users.noreply.github.com>
2021-08-12 15:38:21 +0530

    Add files via upload

---
+++ heylead.js
@@ -0,0 +1,45 @@
+
+function large() {
+    let menudiv = document.getElementById('menu-div');
+    let cancel = document.getElementById('cancel');
+    let links = document.getElementsByClassName('links')[0];
+    let body = document.getElementById('body');
+    links.classList.add('animated');
+    links.style.display = "block";
+    cancel.style.display = "block";
+    menudiv.style.width = "100%";
+    body.style.overflowY = "hidden";
+}
+
+function small() {
+    let menudiv = document.getElementById('menu-div');
+    let cancel = document.getElementById('cancel');
+    let links = document.getElementsByClassName('links')[0];
+    let body = document.getElementById('body');
+    body.style.overflowY = "visible";
+    links.style.display = "none";
+    cancel.style.display = "none";
+    menudiv.style.width = "0%";
+}
+
+
+function left() {
+    let slider1 = document.getElementById('slider1');
+    slider1.scrollTo(1500, 0);
+}
+
+function right() {
+    let slider1 = document.getElementById('slider1');
+    slider1.scrollTo(0, 0);
+}
+
+
+function scrollleft() {
+    let blogcontent = document.getElementById('blog-content');
+    blogcontent.scrollTo(0, 0);
+}
+
+function scrollright() {
+    let blogcontent = document.getElementById('blog-content');
+    blogcontent.scrollTo(1000, 0);
+}
